562fa7e2b44d1c7429035bdced88cf7f09415931
Patrick Rimbault <patrick.rimbault@gmail.com>
2022-03-13 13:01:16 +0200

    Comepleted Day_37

--- 100_days/day_37_habit_tracker/main.py
+++ 100_days/day_37_habit_tracker/main.py
@@ -1,12 +1,66 @@
 import requests
+from datetime import datetime as dt
 
-pixela_endpoint = 'https://pixe.la/v1/user'
+USERNAME = 'patrickrimbault'
+TOKEN = '>Up36cpktm;kX]/@'
+GRAPH_ID = 'graph1'
+
+GRAPH_HARD_LINK = 'https://pixe.la/v1/users/patrickrimbault/graphs/graph1.html'
+
+pixela_endpoint = 'https://pixe.la/v1/users'
 
 user_params = {
-    'token' : '',
-    'username' : 'patrickrimbault',
+    'token' : TOKEN,
+    'username' : USERNAME,
     'agreeTermsOfService' : 'yes',
     'notMinor' : 'yes',
 }
 
-requests.post(pixela_endpoint)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    'id' : 'graph1',
+    'name' : 'Cycling Graph',
+    'unit' : 'Km',
+    'type' : 'float',
+    'color' : 'ajisai',
+}
+
+headers = {
+    'X-USER-TOKEN' : TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+date = dt(year=2022, month=3, day=10)
+
+graph_update = {
+    'date' : date.strftime("%Y%m%d"),
+    'quantity' : '14.2',
+}
+
+# response = requests.post(url=pixel_creation_endpoint, json=graph_update, headers=headers)
+# print(response.text)
+
+PIXEL_DATE = '20220311'
+
+pixel_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{PIXEL_DATE}"
+
+pixel_update = {
+    'quantity' : '42.0',
+}
+
+# response = requests.put(url=pixel_update_endpoint, json=pixel_update, headers=headers)
+# print(response.text)
+
+PIXEL_DELETE_DATE = '20220311'
+
+pixel_delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{PIXEL_DELETE_DATE}"
+
+response = requests.delete(url=pixel_delete_endpoint, headers=headers)
+print(response.text)
